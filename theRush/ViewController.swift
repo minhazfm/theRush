89f51865ef1062de3ae362eab41964f21f650800
Minhaz Mohammad <minhazfm@gmail.com>
2019-08-28 11:30:21 -0400

    Initial commit

---
+++ theRush/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  theRush
+//
+//  Created by Minhaz Mohammad on 8/28/19.
+//  Copyright © 2019 SMPL Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
